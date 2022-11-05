71fda2950a0d49950798d6d4bd72d7d9ddc4dd60
Omolade Hassan <omoladehassan996@gmail.com>
2022-11-05 14:09:43 +0100

    feat: implemented the react-router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Contact from './pages/contact'
+import Footer from "./components/footer"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className='main'>
+    <RouterProvider router={router} />
+    <Footer />
+  </div>
+);
